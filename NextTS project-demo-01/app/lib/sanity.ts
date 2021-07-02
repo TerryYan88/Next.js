eecdca0b367021bddcf47841b0d4544f05fddcf2
Terrance <465446853yan@gmail.com>
2021-07-02 16:45:18 +1000

    setting up sanity database

---
+++ NextTS project-demo-01/app/lib/sanity.ts
@@ -0,0 +1,26 @@
+import {
+    createClient,
+    createPreviewSubscriptionHook,
+    createImageUrlBuilder,
+    createPortableTextComponent,
+} from "next-sanity"
+
+const config = {
+    projectId:"x0wxk0cq",
+    dataset:"production",
+    apiVersion:"2021-03-25",
+    useCdn:false,
+}
+
+export const sanityClient = createClient(config);
+
+export const usePreviewSubscription = createPreviewSubscriptionHook(config);
+
+export const urlFor = (sources: string)=>createImageUrlBuilder(config).image(sources);
+
+export const PortableText = createPortableTextComponent({
+    ...config,
+    serializers:{
+        
+    }
+})
